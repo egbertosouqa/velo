36b36c9adc231e02a2a002d8e958ed6a9f9de842
Egberto Mendonça <egbertosouqa@gmail.com>
2026-05-03 09:03:11 -0300

    Como evitar repetição usando hooks de teste

--- playwright/e2e/pedidos.spec.ts
+++ playwright/e2e/pedidos.spec.ts
@@ -1,80 +1,92 @@
 import { test, expect } from '@playwright/test';
-import {generateOrderCode} from '../support/helpers'
+import { generateOrderCode } from '../support/helpers'
 
 // AAA - Arrange, Act, Assert 
 
-test('deve consultar um pedido aprovado', async ({ page }) => {
-  // Teste Data
+test.describe("Consulta de Pedido", () => {
 
-  const order = 'VLO-AGJAZC'
+  test.beforeAll(async () => {
+    console.log('beforeAll: roda uma vez antes de todos os testes.')
+  })
 
-  // Arrange
-  await page.goto('http://localhost:5173/');
-  // checkpoint
-  await expect(page.getByTestId('hero-section').getByRole('heading')).toContainText('Velô Sprint');
-  await page.getByRole('link', { name: 'Consultar Pedido' }).click();
-  // checkpoint
-  await expect(page.getByRole('heading')).toContainText('Consultar Pedido');
+  test.beforeEach(async ({page}) => {
+    console.log('beforeEach: roda antes de cada teste.')
 
+    // Arrange
+    await page.goto('http://localhost:5173/');
+    // checkpoint
+    await expect(page.getByTestId('hero-section').getByRole('heading')).toContainText('Velô Sprint');
+    await page.getByRole('link', { name: 'Consultar Pedido' }).click();
+    // checkpoint
+    await expect(page.getByRole('heading')).toContainText('Consultar Pedido');
+  })
 
-  // Act
-  await page.getByTestId('search-order-id').fill(order);
-  await page.getByTestId('search-order-button').click();
+  test.afterEach(async () => {
+    console.log('afterEach: roda depois de cada teste.')
+  })
 
-  // Assert
+  test.afterAll(async () => {
+    console.log('afterAll: roda uma vez depois de todos os testes.')
+  })
 
-  // const orderCode = page.locator('//p[text()="Pedido"]/..//p[text()="VLO-AGJAZC"]');
-  // await expect(orderCode).toBeVisible();
+  test('deve consultar um pedido aprovado', async ({ page }) => {
+    // Teste Data
 
-  const containerPedido = page.getByRole('paragraph')
-    .filter({ hasText: /^Pedido$/ })
-    .locator('..') // sobe para o elemento pai (a div que agrupa ambos)
+    const order = 'VLO-AGJAZC'
 
-  await expect(containerPedido).toContainText(order, { timeout: 10_000 });
+    // Act
+    await page.getByTestId('search-order-id').fill(order);
+    await page.getByTestId('search-order-button').click();
 
+    // Assert
 
-  const containerAprovado = page.getByText('APROVADO')
-    .filter({ hasText: /^APROVADO$/ })
-    .locator('..') // sobe para o elemento pai (a div que agrupa ambos)
+    // const orderCode = page.locator('//p[text()="Pedido"]/..//p[text()="VLO-AGJAZC"]');
+    // await expect(orderCode).toBeVisible();
 
-  await expect(containerAprovado).toBeVisible();
+    const containerPedido = page.getByRole('paragraph')
+      .filter({ hasText: /^Pedido$/ })
+      .locator('..') // sobe para o elemento pai (a div que agrupa ambos)
 
-
-});
-
-test('deve exibir mensagem de erro quando o pedido não for encontrado', async ({ page }) => {
-  // Teste Data
-
-  const order =  generateOrderCode() 
-
-
-  // Arrange
-  await page.goto('http://localhost:5173/');
-  await expect(page.getByTestId('hero-section').getByRole('heading')).toContainText('Velô Sprint');
-
-  await page.getByRole('link', { name: 'Consultar Pedido' }).click();
-  await expect(page.getByRole('heading')).toContainText('Consultar Pedido');
-
-  //Act
-  await page.getByTestId('search-order-id').fill(order);
-  await page.getByTestId('search-order-button').click();
+    await expect(containerPedido).toContainText(order, { timeout: 10_000 });
 
 
-  //Assert
+    const containerAprovado = page.getByText('APROVADO')
+      .filter({ hasText: /^APROVADO$/ })
+      .locator('..') // sobe para o elemento pai (a div que agrupa ambos)
 
-  // const title = page.getByRole('heading', { name: 'Pedido não encontrado', level: 3 })
-  // await expect(title).toBeVisible()
+    await expect(containerAprovado).toBeVisible();
 
-  // const msg = page.locator('p', { hasText: 'Verifique o número do pedido e tente novamente' })
-  // await expect(msg).toBeVisible()
 
-  const hMensagem = 'Pedido não encontrado'
-  const pMensagem = 'Verifique o número do pedido e tente novamente'
+  });
 
-  await expect(page.locator('#root')).toMatchAriaSnapshot(`
-    - img
-    - heading "${hMensagem}" [level=3]
-    - paragraph: ${pMensagem}
-    `);
+  test('deve exibir mensagem de erro quando o pedido não for encontrado', async ({ page }) => {
+    // Teste Data
 
+    const order = generateOrderCode()
+
+
+    //Act
+    await page.getByTestId('search-order-id').fill(order);
+    await page.getByTestId('search-order-button').click();
+
+
+    //Assert
+
+    // const title = page.getByRole('heading', { name: 'Pedido não encontrado', level: 3 })
+    // await expect(title).toBeVisible()
+
+    // const msg = page.locator('p', { hasText: 'Verifique o número do pedido e tente novamente' })
+    // await expect(msg).toBeVisible()
+
+    const hMensagem = 'Pedido não encontrado'
+    const pMensagem = 'Verifique o número do pedido e tente novamente'
+
+    await expect(page.locator('#root')).toMatchAriaSnapshot(`
+      - img
+      - heading "${hMensagem}" [level=3]
+      - paragraph: ${pMensagem}
+      `);
+
+  })
 })
+
